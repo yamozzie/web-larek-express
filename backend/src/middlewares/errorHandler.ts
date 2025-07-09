65b933a6c1002405b1e652d502e7864ea6a62f27
Said Alistanov <alistanov.said@yandex.ru>
2025-07-09 12:51:28 +0300

    fixed errorHandler

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,11 +1,14 @@
 import {
   Request, Response, ErrorRequestHandler,
+  NextFunction,
 } from 'express';
 
 const errorHandler: ErrorRequestHandler = (
   err,
   _req: Request,
   res: Response,
+  // eslint-disable-next-line no-unused-vars
+  next: NextFunction,
 ) => {
   const statusCode = err.statusCode || 500;
   const message = statusCode === 500 ? 'Server error' : err.message;
